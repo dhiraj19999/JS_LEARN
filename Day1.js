da8ed99ba228c03791985208bc20591bbdb42641
dhiraj19999 <yogeshsamse682@gmail.com>
2023-12-03 19:34:50 +0530

    add

--- Day1.js
+++ Day1.js
@@ -788,9 +788,11 @@ How js works
 */
 
 
-
+console.log(this);
+console.log(window);
+console.log(firsname);
 var  firsname="dhiraj"
-
+console.log(firsname);
 /*
 at first code get complied and then excute
 
@@ -802,4 +804,95 @@ firsname varible avallible in global scope as it did not decalred in function
 in js code excutes inside excution context and the first excution contest is global
 excution context
 
+global excution has two phases
+first is creation phase
+second is code excution phase
+when globel excution context get created in creation phase then there is creation of varible for example
+firsname varible get created with undefined value as it declare with var keyword
+in creatin phase the value of this keyword get set with  value windows ..(this:window)
+window is object here
+
+now come to excution phase
+
+at first it prints window object as this sets its value to window
+then window obj get printed
+now at 3rd line it prints undefined as firstname set value with undefined in creation phase
+now in 4th line firsname value get set with dhiraj .
+at 5th line dhiraj get printed
 */
+
+/*
+what happens when decleare function
+*/
+
+console.log(this);
+console.log(window);
+console.log(myFunction);
+myFunction()
+console.log(FullName);
+
+function myFunction(){
+    console.log("hello");
+}
+
+var firsname="dhiraj"
+var lastname="garad"
+var FullName=firsname+lastname
+
+/*
+firstname,lastname and Fullname varibles and function myfunction are availbel in global scope
+ code get compiled for syntex error checking
+during global excutin context creation  this keyword with window
+then firstname,lastname,fullname and myfunction get created .
+varibles get created with undefined value
+
+then excution phase at line no 830 it prints just total function not value for examplee
+it prints function but not the console.log() value
+at line no 831 it prints hello
+*/
+
+
+console.log(myfun);
+
+var myfun=function(){
+
+    console.log("fun");
+}
+
+console.log(myfun);
+/*
+
+myfun availble in global scope
+global exction creation get created and with this myfun is get created with undefined value
+as it declre with var keyword
+
+during excution phase line no 855 prints undefined
+at line no 857 myfun value get changed to function
+line no 862 prints function
+*/
+
+// let and const
+
+console.log(fruit);
+
+let fruit="mango"
+console.log(fruit);
+
+/*
+during global excution creation fruit get created with unintilised 
+
+during excution phase lone no 876 prints cannot accses fruit before intialisation
+line no 878 it intialize the value
+
+
+
+
+*/
+
+console.log(app);// it prints app is not defined becuse we not define any app varible
+
+let zomb;
+log(zomb) // it prints undefined
+
+const kpm;
+console.log(kpm); // syntex error missing intiliazer in const declration
