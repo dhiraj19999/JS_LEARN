913aac9e82a6ce25037bd2009ad73342498165b2
Dhiraj Garad <yogeshsamse682@gmail.com>
2024-07-31 23:20:33 +0530

    Create Three.js

---
+++ Teaching/Three.js
@@ -0,0 +1,524 @@
+// important array methods 
+
+ const numbers = [4,2,5,8];
+
+// function myFunc(number, index){
+//     console.log(`index is ${index} number is ${number}`);
+// }
+
+// numbers.forEach(function(number,index){
+//     console.log(`index is ${index} number is ${number}`);
+// });
+
+// numbers.forEach(function(number, index){
+//     console.log(number*3, index);
+// })
+
+const users = [
+    {firstName: "harshit", age: 23},
+    {firstName: "mohit", age: 21},
+    {firstName: "nitish", age: 22},
+    {firstName: "garima", age: 20},
+]
+
+// users.forEach(function(user){
+//     console.log(user.firstName);
+// });
+
+// users.forEach((user, index)=>{
+//     console.log(user.firstName, index);
+// })
+
+// for(let user of users){
+//     console.log(user.firstName);
+// }
+
+// map method 
+// const numbers = [3,4,6,1,8];
+
+// const square = function(number){
+//     return number*number;
+// }
+
+// const squareNumber = numbers.map((number, index)=>{
+//     return index;
+// });
+// console.log(squareNumber);
+
+const users = [
+    {firstName: "harshit", age: 23},
+    {firstName: "mohit", age: 21},
+    {firstName: "nitish", age: 22},
+    {firstName: "garima", age: 20},
+]
+
+const userNames = users.map((user)=>{
+    return user.firstName;
+});
+
+console.log(userNames);
+// filter method 
+
+const numbers = [1,3,2,6,4,8];
+
+
+
+const evenNumbers = numbers.filter((number)=>{
+    return number % 2 === 0;
+});
+console.log(evenNumbers);// reduce 
+const numbers = [1,2,3,4,5, 10];
+
+// aim : sum of all the numbers in array 
+
+// const sum = numbers.reduce((accumulator, currentValue)=>{
+//     return accumulator + currentValue;
+// }, 100);
+
+// console.log(sum);
+// accumulator , currentValue,  return 
+// 1               2              3 
+// 3               3              6
+// 6               4              10
+// 10              5              15
+// 15              10             25
+
+
+// const userCart = [
+//     {productId: 1, productName: "mobile", price: 12000},
+//     {productId: 2, productName: "laptop", price: 22000},
+//     {productId: 3, productName: "tv", price: 15000},
+// ]
+
+// const totalAmount = userCart.reduce((totalPrice, currentProduct)=>{
+//     return totalPrice + currentProduct.price;
+// }, 0)
+
+// console.log(totalAmount);
+
+// total price      currentValue     return 
+// 0                {}                  12000
+// 12000            22000                34000
+// 34000            15000                49000
+// sort  
+
+// 5,9,1200, 400, 3000
+// 5, 9, 400, 1200, 3000 (expected)
+
+// ["5", "9", "1210", "410", "3000"] 
+// [53, 57, 49, 52, 51]
+
+// const userNames = ['harshit', 'abcd', 'mohit', 'nitish', 'aabc', 'ABC', 'Harshit'];
+// userNames.sort();
+// console.log(userNames);
+
+// const numbers = [5,9,1200, 410, 3000];
+// numbers.sort((a,b)=>{
+//     return b-a;
+// });
+// numbers.sort((a,b)=>a-b);
+// console.log(numbers);
+
+// 1200,410 
+// a-b ---> 790
+// a-b ---> postive (greater than 0) ---> b, a
+// 410 , 1200
+
+// a-b ---> negative ----> a,b
+// 5, 9 ---> -4 
+// price lowToHigh HighToLow 
+const products = [
+    {productId: 1, produceName: "p1",price: 300 },
+    {productId: 2, produceName: "p2",price: 3000 },
+    {productId: 3, produceName: "p3",price: 200 },
+    {productId: 4, produceName: "p4",price: 8000 },
+    {productId: 5, produceName: "p5",price: 500 },
+]
+
+// lowToHigh
+const lowToHigh = products.slice(0).sort((a,b)=>{
+    return a.price-b.price
+});
+
+const highToLow = products.slice(0).sort((a,b)=>{
+    return b.price-a.price;
+});
+
+
+
+const users = [
+    {firstName: "harshit", age: 23},
+    {firstName: "mohit", age: 21},
+    {firstName: "nitish", age: 22},
+    {firstName: "garima", age: 20},
+]
+
+
+users.sort((a,b)=>{
+    if(a.firstName > b.firstName){
+        return 1;
+    }else{
+        return -1;
+    }
+});
+
+console.log(users);
+// sort  
+
+// 5,9,1200, 400, 3000
+// 5, 9, 400, 1200, 3000 (expected)
+
+// ["5", "9", "1210", "410", "3000"] 
+// [53, 57, 49, 52, 51]
+
+// const userNames = ['harshit', 'abcd', 'mohit', 'nitish', 'aabc', 'ABC', 'Harshit'];
+// userNames.sort();
+// console.log(userNames);
+
+// const numbers = [5,9,1200, 410, 3000];
+// numbers.sort((a,b)=>{
+//     return b-a;
+// });
+// numbers.sort((a,b)=>a-b);
+// console.log(numbers);
+
+// 1200,410 
+// a-b ---> 790
+// a-b ---> postive (greater than 0) ---> b, a
+// 410 , 1200
+
+// a-b ---> negative ----> a,b
+// 5, 9 ---> -4 
+// price lowToHigh HighToLow 
+const products = [
+    {productId: 1, produceName: "p1",price: 300 },
+    {productId: 2, produceName: "p2",price: 3000 },
+    {productId: 3, produceName: "p3",price: 200 },
+    {productId: 4, produceName: "p4",price: 8000 },
+    {productId: 5, produceName: "p5",price: 500 },
+]
+
+// lowToHigh
+const lowToHigh = products.slice(0).sort((a,b)=>{
+    return a.price-b.price
+});
+
+const highToLow = products.slice(0).sort((a,b)=>{
+    return b.price-a.price;
+});
+
+
+
+const users = [
+    {firstName: "harshit", age: 23},
+    {firstName: "mohit", age: 21},
+    {firstName: "nitish", age: 22},
+    {firstName: "garima", age: 20},
+]
+
+
+users.sort((a,b)=>{
+    if(a.firstName > b.firstName){
+        return 1;
+    }else{
+        return -1;
+    }
+});
+
+console.log(users);// optional chaining 
+
+const user  = {
+    firstName: "harshit",
+    // address: {houseNumber: '1234'}
+}
+
+
+
+console.log(user?.firstName);
+console.log(user?.address?.houseNumber);// methods
+// function inside object
+
+function personInfo(){
+    console.log(`person name is ${this.firstName} and age is ${this.age}`);
+}
+
+const person1 = {
+    firstName : "harsh",
+    age: 8,
+    about: personInfo
+}
+const person2 = {
+    firstName : "mohit",
+    age: 18,
+    about: personInfo
+}
+const person3 = {
+    firstName : "nitish",
+    age: 17,
+    about: personInfo
+}
+
+person1.about();
+person2.about();
+person3.about();
+/ console.log(window);
+// "use strict";
+// function myFunc(){
+    
+//     console.log(this);
+// }
+// myFunc();
+
+
+function about(hobby, favMusician){
+    console.log(this.firstName, this.age, hobby, favMusician);
+}
+const user1 = {
+    firstName : "harshit",
+    age: 8,   
+}
+const user2 = {
+    firstName : "mohit",
+    age: 9,
+    
+}
+
+// apply
+// about.apply(user1, ["guitar", "bach"]);
+// const func = about.bind(user2, "guitar", "bach");
+// func();
+
+
+const user1 = {
+    firstName : "harshit",
+    age: 8,
+    about: function(){
+        console.log(this.firstName, this.age);
+    }   
+}
+
+// don't do this mistake 
+
+// user1.about();
+const myFunc = user1.about.bind(user1);
+myFunc();
+// arrow functions 
+
+const user1 = {
+    firstName : "harshit",
+    age: 8,
+    about: () => {
+        console.log(this.firstName, this.age);
+    }   
+}
+
+user1.about(user1);
+
+ const user1 = {
+//     firstName : "harshit",
+//     age: 8,
+//     about: function(){
+//         console.log(this.firstName, this.age);
+//     }   
+// }
+
+// const user1 = {
+//     firstName : "harshit",
+//     age: 8,
+//     about(){
+//         console.log(this.firstName, this.age);
+//     }   
+// }
+
+
+
+user1.about();
+
+
+const userMethods = {
+    about : function(){
+        return `${this.firstName} is ${this.age} years old.`;
+    },
+    is18 : function(){
+        return this.age >= 18;
+    },
+    sing: function(){
+        return 'toon na na na la la ';
+    }
+}
+function createUser(firstName, lastName, email, age, address){
+    const user = Object.create(userMethods);// {}
+    user.firstName = firstName;
+    user.lastName = lastName;
+    user.email = email;
+    user.age = age;
+    user.address = address;
+    return user;
+}
+
+const user1 = createUser('harshit', 'vashsith', 'harshit@gmail.com', 9, "my address");
+const user2 = createUser('harsh', 'vashsith', 'harshit@gmail.com', 19, "my address");
+const user3 = createUser('mohit', 'vashsitha', 'harshit@gmail.com', 17, "my address");
+console.log(user1);
+console.log(user1.about());
+// console.log(user3.sing());const obj1 = {
+    key1: "value1",
+    key2: "value2"
+}
+
+// __proto__
+
+// offical ecmascript documentation 
+
+// [[prototype]]
+
+// __proto__ , [[prototype]]
+
+
+
+// prototype    
+
+const obj2 = Object.create(obj1); // {}
+// there is one more way to create empty object
+obj2.key3 = "value3";
+// obj2.key2 = "unique";
+console.log(obj2);
+
+console.log(obj2.__proto__);
+function hello(){
+    console.log("hello world");
+}
+
+// javascript function ===> function  + object
+
+// console.log(hello.name);
+
+// you can add your own properties 
+// hello.myOwnProperty = "very unique value";
+// console.log(hello.myOwnProperty);
+
+// name property ---> tells function name;
+
+// function provides more usefull properties.
+
+
+// console.log(hello.prototype); // {}
+
+// only functions provide prototype property
+
+// hello.prototype.abc = "abc";
+// hello.prototype.xyz = "xyz";
+// hello.prototype.sing = function(){
+//     return "lalalla";
+// };
+// console.log(hello.prototype.sing());
+/ const userMethods = {
+//     about : function(){
+//         return `${this.firstName} is ${this.age} years old.`;
+//     },
+//     is18 : function(){
+//         return this.age >= 18;
+//     },
+//     sing: function(){
+//         return 'toon na na na la la ';
+//     }
+// }
+function createUser(firstName, lastName, email, age, address){
+    const user = Object.create(createUser.prototype);// {}
+    user.firstName = firstName;
+    user.lastName = lastName;
+    user.email = email;
+    user.age = age;
+    user.address = address;
+    return user;
+}
+createUser.prototype.about = function(){
+    return `${this.firstName} is ${this.age} years old.`;
+};
+createUser.prototype.is18 = function (){
+    return this.age >= 18; 
+}
+createUser.prototype.sing = function (){
+    return "la la la la ";
+}
+
+
+const user1 = createUser('harshit', 'vashsith', 'harshit@gmail.com', 18, "my address");
+const user2 = createUser('harsh', 'vashsith', 'harshit@gmail.com', 19, "my address");
+const user3 = createUser('mohit', 'vashsitha', 'harshit@gmail.com', 17, "my address");
+console.log(user1);
+console.log(user1.is18());
+// new keyword 
+// 1.) this = {}
+// 2.) return {} 
+//
+
+// __proto__ 
+// // official ecmascript document
+// [[prototype]]
+
+// constructor function 
+function CreateUser(firstName, lastName, email, age, address){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.age = age;
+    this.address = address;
+}
+CreateUser.prototype.about = function(){
+    return `${this.firstName} is ${this.age} years old.`;
+};
+CreateUser.prototype.is18 = function (){
+    return this.age >= 18; 
+}
+CreateUser.prototype.sing = function (){
+    return "la la la la ";
+}
+
+
+const user1 = new CreateUser('harshit', 'vashsith', 'harshit@gmail.com', 18, "my address");
+const user2 = new CreateUser('harsh', 'vashsith', 'harshit@gmail.com', 19, "my address");
+const user3 = new CreateUser('mohit', 'vashsitha', 'harshit@gmail.com', 17, "my address");
+console.log(user1);
+console.log(user1.is18());function CreateUser(firstName, lastName, email, age, address){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.age = age;
+    this.address = address;
+}
+CreateUser.prototype.about = function(){
+    return `${this.firstName} is ${this.age} years old.`;
+};
+CreateUser.prototype.is18 = function (){
+    return this.age >= 18; 
+}
+CreateUser.prototype.sing = function (){
+    return "la la la la ";
+}
+
+
+const user1 = new CreateUser('harshit', 'vashsith', 'harshit@gmail.com', 18, "my address");
+const user2 = new CreateUser('harsh', 'vashsith', 'harshit@gmail.com', 19, "my address");
+const user3 = new CreateUser('mohit', 'vashsitha', 'harshit@gmail.com', 17, "my address");
+
+for(let key in user1){
+    // console.log(key);
+    if(user1.hasOwnProperty(key)){
+        console.log(key);
+    }
+
+}
+// let numbers = [1,2,3];
+
+// // console.log(Object.getPrototypeOf(numbers));
+// console.log(Array.prototype);
+// console.log(numbers);
+
+// function hello(){
+//     console.log("hello");
+// }
+
+
+
+
